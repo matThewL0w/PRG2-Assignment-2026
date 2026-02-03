0aec8eff5b742716d8893d0f2238a64386b63a1c
Hua-Jay <lee_hua_jay@s2021.ssts.edu.sg>
2026-02-04 05:32:21 +0800

    sleep

--- Assignment/Restaurant.cs
+++ Assignment/Restaurant.cs
@@ -13,7 +13,7 @@ namespace Assignment
         public string restaurantEmail { get; set; }
         public List<Menu> menus { get; set; }
         private List<Order> orders = new List<Order>();
-        public List<Order> Orders { get; set; }
+        public Queue<Order> orderQueue { get; set; }
         private List<SpecialOffer> specialOffers = new List<SpecialOffer>();
         public List<SpecialOffer> SpecialOffers {get; set;}
         public Restaurant() { }
