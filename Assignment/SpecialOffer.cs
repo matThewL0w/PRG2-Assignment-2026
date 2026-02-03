12a8f71468e06f7daeb47c294dc66005d8570cda
Hua-jay <lee_hua_jay@s2021.ssts.edu.sg>
2026-02-03 15:54:47 +0800

    special offers class

---
+++ Assignment/SpecialOffer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    internal class SpecialOffer
+    {
+        public string offerCode { get; set; }
+        public string offerDesc { get; set; }
+        public SpecialOffer(string OfferCode, string OfferDesc)
+        {
+            offerCode = OfferCode;
+            offerDesc = OfferDesc;
+        }
+        public override string ToString()
+        {
+            return $"Offer Code: {offerCode}, Offer Description: {offerDesc}";
+        }
+    }
+}
