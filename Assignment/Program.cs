81ae937ae9be46a83220052879cb815f5ebe7b45
Matthew Low <matthewlowyongjin@gmail.com>
2026-02-06 19:36:54 +0800

    Feature 8 (Remove)

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -16,6 +16,7 @@ List<Restaurant> restaurants = new List<Restaurant>();
 List<Customer> customers = new List<Customer>();
 List<FoodItem> foodlist = new List<FoodItem>();
 List<Order> orderlist = new List<Order>();
+Stack<Order> refundStack = new Stack<Order>();
 
 using (StreamReader reader = new StreamReader("restaurants.csv"))
 {
@@ -145,7 +146,7 @@ void DisplayAllOrders()
         {
             string customerName = "Unknown";
 
-            // Find which customer owns this order
+            // FINDER
             foreach (Customer customer in customers)
             {
                 if (customer.orders.Contains(order))
@@ -166,4 +167,64 @@ void DisplayAllOrders()
         }
     }
 }
-DisplayAllOrders();
+
+void RemoveExistingOrder()
+{
+    Customer CorrectCustomer = null;
+    Console.WriteLine("Delete Order");
+    Console.WriteLine("============");
+    Console.Write("Enter Customer Email: ");
+    string customerEmail = Console.ReadLine();
+    foreach (Customer customer in customers)
+    {
+        if (customerEmail == customer.emailAddress)
+        {
+            CorrectCustomer = customer;
+            break;
+        }
+    }
+    Console.WriteLine("Pending Orders:");
+    foreach (Order order in CorrectCustomer.orders)
+    {
+        if (order.orderStatus == "Pending")
+        {
+            Console.WriteLine(order.orderId);
+        }
+    }
+    Console.Write("Enter Order ID: ");
+    int orderId = Convert.ToInt32(Console.ReadLine());
+    Order SelectedOrder = null;
+    foreach (Order order in CorrectCustomer.orders)
+    {
+        if (orderId == order.orderId)
+        {
+            SelectedOrder = order;
+            break;
+        }
+    }
+    Console.WriteLine();
+    Console.WriteLine($"Customer: {CorrectCustomer.customerName}");
+    Console.WriteLine("Ordered Items:");
+    SelectedOrder.DisplayFoodItems();
+    Console.WriteLine($"Delivery date/time: {SelectedOrder.orderDateTime:dd/MM/yyyy HH:mm}");
+    Console.WriteLine($"Total Amount: ${SelectedOrder.orderTotal:F2}");
+    Console.WriteLine($"Order Status: {SelectedOrder.orderStatus}");
+    Console.Write("Confirm deletion? [Y/N]: ");
+    string confirm = Console.ReadLine();
+    if (confirm.ToUpper() == "Y")
+    {
+        SelectedOrder.orderStatus = "Cancelled";
+        refundStack.Push(SelectedOrder);
+
+        Console.WriteLine(
+            $"Order {SelectedOrder.orderId} cancelled. " +
+            $"Refund of ${SelectedOrder.orderTotal:F2} processed."
+        );
+    }
+    else
+    {
+        Console.WriteLine("Order deletion cancelled.");
+        return;
+    }
+
+}
